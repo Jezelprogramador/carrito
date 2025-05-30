49213b3724db6572d0b8adaffb5c54e4770e4bf2
Jezelprogramador <jeztorres19@gmail.com>
2025-05-30 16:42:10 +0000

    Aquí describe los cambios que hiciste

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     async function sendCommandToBackend(dataValue, type) {
         // ★★★ URL DEL BACKEND CON TU IP PÚBLICA DE EC2 ★★★
-        const backendUrl = 'https://54.227.33.244/carrito1/command.php'; 
+        const backendUrl = 'https://3.89.60.55/carrito1/command.php'; 
         
         const formData = new FormData();
         let consoleIdent = "- v_FINAL";
